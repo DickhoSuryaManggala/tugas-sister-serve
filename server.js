a3429904e7bc059a33e8d1d04109709440f965c6
DickhoSuryaManggala <111202315323@mhs.dinus.ac.id>
2026-01-11 21:01:15 +0700

    Fix routing for Vercel

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/config', (req, res) => {
     res.json({
